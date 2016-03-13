e8cf0680c4d6219efef5a273978133642ce9df9a
anvaka <anvaka@gmail.com>
2016-03-12 19:18:20 -0800

    First version

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
 # tiny.xml
 
-Tiny in-browser xml parser
+Tiny in-browser xml parser. This module is not supposed to work from node.js
+If you want node.js version [try something else](https://anvaka.github.io/npmrank/online/#tag=xml)
+
+# usage
+
+``` js
+var xmlString = '<?xml version="1.0" ?>' +
+ '<items xmlns="http://foo.com">' +
+ ' <item>Foo</item>' +
+ ' <item color="green">Bar</item>' +
+ '</items>'
+
+var parser = require('tiny.xml')(xmlString)
+var nodes = parser.selectNodes('item')
+assert(nodes.length === 2, 'it found two nodes')
+
+// Nodes are actual browser elements: https://developer.mozilla.org/en-US/docs/Web/API/Element
+var firstNode = nodes[0]
+assert(firstNode instanceof Element)
+
+// Since we are using standard browser API, powerful Element interface is supported:
+var root = parser.selectNodes('items')[0]
+
+var all = root.querySelectorAll('item')
+assert(all.length === 2, 'matches all item elements')
+
+var greenNode = root.querySelector('item[color="green"]')
+assert(greenNode.length === 1, 'picks only one green node')
+```
 
 # license
 
---
+++ index.js
@@ -0,0 +1,89 @@
+/* globals XPathResult ActiveXObject DOMParser*/
+module.exports = parser;
+
+function parser(xml) {
+  if (typeof document === 'undefined') {
+    // Since we are using DOMParser/Microsoft.XMLDOM API, this will not work in node:
+    throw new Error('tiny.xml supports only browser environment')
+  }
+
+  if (typeof document.evaluate === 'function') {
+    // IE Has it's own special way...
+    return nonIEParser(xml);
+  } else {
+    return ieParser(xml);
+  }
+}
+
+function nonIEParser(xml) {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(xml, 'text/xml');
+  var nameSpaces = extractNodeNamespaces(doc.documentElement);
+
+  return {
+    selectNodes: function (name, startFrom, nsPrefix) {
+      nsPrefix = nsPrefix || 'x';
+      if (!nameSpaces[nsPrefix]) { return []; }
+
+      var root = startFrom || doc;
+      var xpathResult = doc.evaluate('.//' + nsPrefix + ':' + name, root, nsResolver, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+      var result = [];
+
+      for (var i = 0; i < xpathResult.snapshotLength; i++) {
+        result.push(xpathResult.snapshotItem(i));
+      }
+
+      return result;
+    },
+
+    getText: function (node) {
+      return node && node.textContent;
+    }
+  };
+
+  function nsResolver(prefix) {
+    return nameSpaces[prefix] || null;
+  }
+}
+
+function ieParser(xml) {
+  var doc = new ActiveXObject('Microsoft.XMLDOM');
+  doc.setProperty('SelectionLanguage', 'XPath');
+  doc.loadXML(xml);
+  var nameSpaces = extractNodeNamespaces(doc.documentElement);
+
+  var ns = Object.keys(nameSpaces).map(function (x) { return 'xmlns:' + x + "='" + nameSpaces[x] + "'"; }).join(' ');
+  doc.setProperty('SelectionNamespaces', ns);
+
+  return {
+    selectNodes: function(name, startFrom, nsPrefix) {
+      nsPrefix = nsPrefix || 'x';
+      if (!nameSpaces[nsPrefix]) { return []; }
+
+      var ctx = startFrom || doc;
+      var selectNodeResult = ctx.selectNodes('.//' + nsPrefix + ':' + name);
+      var result = [];
+      for (var i = 0; i < selectNodeResult.length; ++i) {
+        result.push(selectNodeResult[i]);
+      }
+      return result;
+    },
+
+    getText: function (node) {
+      return node && node.text;
+    }
+  };
+}
+
+function extractNodeNamespaces(node) {
+  var result = {};
+  for (var i = 0; i < node.attributes.length; ++i) {
+    var attr = node.attributes[i];
+    if (attr.name.match(/^xmlns/)) {
+      var parts = attr.name.split(':');
+      var prefx = parts.length === 1 ? 'x' : parts[1];
+      result[prefx] = attr.value;
+    }
+  }
+  return result;
+}
