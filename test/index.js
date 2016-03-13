e1c8b6ee316814aae71010ea3914331dafc94134
anvaka <anvaka@gmail.com>
2016-03-13 13:15:21 -0700

    Added xpath test

--- test/index.js
+++ test/index.js
@@ -1,11 +1,12 @@
 /* global describe, it, tinyxml, expect */
 describe('parser', function() {
-  it('should parse simple text',function () {
-    var xmlString = '<?xml version="1.0" ?>' +
-    '<items xmlns="http://foo.com">' +
-    ' <item>Foo</item>' +
-    ' <item color="green">Bar</item>' +
-    '</items>'
+  var xmlString = '<?xml version="1.0" ?>' +
+  '<items xmlns="http://foo.com">' +
+  ' <item>Foo</item>' +
+  ' <item color="green">Bar</item>' +
+  '</items>'
+
+  it('should parse simple text', function () {
     var parser = tinyxml(xmlString);
     var nodes = parser.selectNodes('items');
     expect(nodes.length).toBe(1)
@@ -16,6 +17,14 @@ describe('parser', function() {
     expect(all.length).toBe(2)
 
     var greenNode = root.querySelector('item[color="green"]')
+    expect(greenNode.textContent).toBe('Bar')
+  })
+
+  it('can use xpath to select nodes', function() {
+    var parser = tinyxml(xmlString);
+    var nodes = parser.selectNodes('*[@color="green"]')
+    expect(nodes.length).toBe(1)
+    var greenNode = nodes[0]
     expect(greenNode.textContent).toBe('Bar')
   })
 })
